9156ff10642f51eaad873b33b439ceb525311d61
VaniliaVillam <viragmarta02@gmail.com>
2024-03-07 14:04:40 +0100

    csut

--- Video.js
+++ Video.js
@@ -26,6 +26,10 @@ const Video = () => {
     <View style={{flex: 1, padding: 24}}>
         <WebView source={{ uri: 'https://www.youtube.com/embed/mwGDDM7ZDyc?si=QF-vfQf202vVd-Kc' }} style={{ flex: 1 , margin:20 }} />
         <WebView source={{ uri: 'https://www.youtube.com/embed/wtFPIOV2bWM?si=AHt3phKTOPuVfD3y' }} style={{ flex:1, margin:20}} />
+        <WebView source={{ uri: 'https://www.youtube.com/embed/sD-tzrVZvrY?si=nswedOKuC0gWuQHC' }} style={{ flex:1, margin:20}} />
+        <WebView source={{ uri: 'https://www.youtube.com/embed/pfNPFeoE3c0?si=BQFacwp1_0FCLvs_' }} style={{ flex:1, margin:20}} />
+        <WebView source={{ uri: 'https://www.youtube.com/embed/iM48VDuPRQk?si=VuK7eVcVBXUNjNFi' }} style={{ flex:1, margin:20}} />
+        <WebView source={{ uri: 'https://www.youtube.com/embed/JU420IYFp50?si=FK2JcaQAUJ7hbRnA' }} style={{ flex:1, margin:20}} />
 
      
     </View>
@@ -33,3 +37,5 @@ const Video = () => {
 };
 
 export default Video;
+
+
